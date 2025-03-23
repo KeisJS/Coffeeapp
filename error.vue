2ec5a1b4de6eff3ae683fada3e8f1632afc631e3
vitaly <workicio@mail.ru>
2025-03-23 16:28:02 +0300

    T-2: add login, account, error pages

---
+++ error.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const { error } = defineProps({
+  error: Object as () => NuxtError
+})
+
+if (error && error?.statusCode === 404) {
+  navigateTo('/login')
+}
+</script>
+
+<template>
+  <NuxtLayout name="single-content">
+    <h1>{{ error?.statusCode }}</h1>
+    <div>
+      {{ error }}
+    </div>
+  </NuxtLayout>
+</template>
